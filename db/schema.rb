625c924c964045df857e3ff9ec705f2c42b865a3
Andrew Johnston <av.johnst@gmail.com>
2021-02-02 16:51:12 -0700

    Fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_02_02_223339) do
 
-ActiveRecord::Schema.define(version: 2021_02_02_222005) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
