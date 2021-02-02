7e60be603ad2dc9c1a333811406b4b9c72cb55e3
Andrew Johnston <av.johnst@gmail.com>
2021-02-02 16:41:18 -0700

    Finish parent CRUD functionality

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,18 +4,38 @@ class TeamsController < ApplicationController
   end
 
   def show
-
+    @team = Team.find(params[:id])
   end
 
   def new
 
   end
 
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def destroy
+    Team.destroy(params[:id])
+    redirect_to '/teams'
+  end
+
+  def update
+    team = Team.find(params[:id])
+    team.update({
+      name: params[:team][:name],
+      city: params[:team][:city],
+      updated_at: Time.now
+      })
+    team.save
+    redirect_to "/teams/#{team.id}"
+  end
+
   def create
     team = Team.new({
       name: params[:team][:name],
       city: params[:team][:city],
-      rank: params[:team][:id],
+      rank: Team.all.count + 1,
       first_place: false,
       created_at: Time.now,
       updated_at: Time.now
