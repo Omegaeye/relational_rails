0bd4c4d4483a4650c787ea2959b6f8c81e0694d5
Andrew Johnston <av.johnst@gmail.com>
2021-02-02 14:24:01 -0700

    Get the parent show and create started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'intro#index'
+
   get '/teams', to: 'teams#index'
+  get '/teams/new', to: 'teams#new'
+  get '/teams', to: 'teams#create'
+  # get '/teams/:id', to: "teams#show"
 end
