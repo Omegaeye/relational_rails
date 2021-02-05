afbedf2a633029923bad3ec5d225ca7e80e32393
Khoa Nguyen <Khoang323@hotmail.com>
2021-02-05 14:03:54 -0700

    Finished hero_edit_spec

---
+++ spec/features/heroes/edit_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "heroes edit page" do
+  it "parent index has update link and it updates parent" do
+    hero = Hero.create(name: 'Hero', alive: true, level: 1)
+    mount = hero.mounts.create(name: "Mount", level: 20)
+    visit "/heroes/#{hero.id}"
+
+    expect(page).to have_button("Update Hero")
+    click_button "Update Hero"
+    expect(current_path).to eq("/heroes/#{hero.id}/edit")
+    fill_in "name", with: "Hero 2"
+    click_button "submit"
+    expect(current_path).to eq("/heroes/#{hero.id}")
+    expect(page).to have_content("Hero 2")
+    expect(page).to have_content("#{hero.level}")
+    # expect(page).to have_content("#{hero.alive}")
+    expect(page).to have_content("#{hero.created_at}")
+    expect(page).to have_content("#{hero.updated_at}")
+  end
+end
