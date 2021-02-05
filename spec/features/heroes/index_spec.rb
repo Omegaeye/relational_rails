68132a58a8df232b22bc2e394a85243a6dacca86
Khoa Nguyen <Khoang323@hotmail.com>
2021-02-05 11:05:49 -0700

    Finished show_spec

--- spec/features/heroes/index_spec.rb
+++ spec/features/heroes/index_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "heroes index page", type: :feature do
   it "parent index page shows list of parents" do
-    hero = Hero.create(name: 'Hero')
-
+    hero = Hero.create(name: "Hero2", level: 1, alive: true)
+    mount = hero.mounts.create(name: "Mount", level: 20, )
     visit '/heroes'
 
-    expect(page).to have_content(hero.name)
+    expect(page).to have_link("#{hero.name}")
+    expect(page).to have_content("#{hero.level}")
+    expect(page).to have_content("#{hero.alive}")
   end
 
 end
--- spec/features/heroes/show_spec.rb
+++ spec/features/heroes/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "heroes show page", type: :feature do
   it "parent show page shows list of parents and all attributes" do
     hero = Hero.create(name: 'Hero', alive: false, level: 1)
+    mount = hero.mounts.create(name: "Mount", level: 20, )
 
     visit "/heroes/#{hero.id}"
 
@@ -11,6 +12,21 @@ RSpec.describe "heroes show page", type: :feature do
     expect(page).to have_content(hero.level)
     expect(page).to have_content(hero.created_at)
     expect(page).to have_content(hero.updated_at)
+    expect(page).to have_button("Update Hero")
+    click_button "Update Hero"
+    expect(current_path).to eq("/heroes/#{hero.id}/edit")
+
+    visit "/heroes/#{hero.id}"
+
+    click_button "Add New Mount"
+    expect(current_path).to eq("/heroes/#{hero.id}/mounts/new")
+
+    visit "/heroes/#{hero.id}"
+
+    expect(page).to have_link("Mounts")
+    click_link "Mounts"
+    expect(current_path).to eq("/heroes/#{hero.id}/mounts")
+
   end
 
 end
