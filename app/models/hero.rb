f84f6316c13beec5a08888a556ac4eb8f227b40c
Khoa Nguyen <Khoang323@hotmail.com>
2021-02-06 12:16:21 -0700

    Heroes params

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,10 @@
 class Hero < ApplicationRecord
   has_many :mounts, :dependent => :destroy
+  validates :name, presence: true
+
+  def self.order_by_creation_time
+    order('created_at ASC')
+  end
+
+
 end
