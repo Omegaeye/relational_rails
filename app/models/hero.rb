501b535e440f5574cee05ebb3cc55f5d10e6ce63
Khoa Nguyen <Khoang323@hotmail.com>
2021-02-07 10:50:04 -0700

    Cleaning up

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,10 +2,6 @@ class Hero < ApplicationRecord
   has_many :mounts, :dependent => :destroy
   validates :name, presence: true
 
-  def self.order_by_creation_time
-    order('created_at ASC')
-  end
-
   def self.sorted
     self.all.sort_by do |hero|
       hero.name.downcase
