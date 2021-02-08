c6dd4421337a9031c8b194b683d43b62f42af84b
Khoa Nguyen <Khoang323@hotmail.com>
2021-02-07 21:20:41 -0700

    Finished with extensions and test coverage is 100%

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,10 +2,16 @@ class Hero < ApplicationRecord
   has_many :mounts, :dependent => :destroy
   validates :name, presence: true
 
-  def self.sorted
+  def self.sorted_by_created_at
     self.all.sort_by do |hero|
-      hero.name.downcase
+      hero.created_at
     end
+  end
+
+  def self.number_of_mounts
+    self.all.sort_by do |hero|
+      hero.mounts.count
+    end.reverse
   end
 
   def mount_count
--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -12,9 +12,27 @@ RSpec.describe Hero do
       hero2 = Hero.create(name: "Steve", level: 7)
       mount2 = hero2.mounts.create(name: "kupo", level: 34)
 
-      expect(Hero.sorted).to eq([hero1, hero2])
+      expect(Hero.sorted_by_created_at).to eq([hero1, hero2])
     end
+  end
 
+  it 'number of mounts' do
+    Hero.destroy_all
+    Mount.destroy_all
+    hero1 = Hero.create(name: "Hero", level: 8)
+    hero2 = Hero.create(name: "Andrew", level: 9)
+    mount1 = hero1.mounts.create(name: "Andrew", level: 20, fly: true)
+    mount2 = hero1.mounts.create(name: "Khoa", level: 34, fly: true)
+    mount3 = hero2.mounts.create(name: "Janis", level: 25, fly: true)
+
+    expect(Hero.number_of_mounts).to eq([hero1, hero2])
+
+    hero3 = Hero.create(name: "Hero3", level: 8)
+    mount1 = hero3.mounts.create(name: "Andrew", level: 20, fly: true)
+    mount2 = hero3.mounts.create(name: "Khoa", level: 34, fly: true)
+    mount3 = hero3.mounts.create(name: "Janis", level: 25, fly: true)
+
+    expect(Hero.number_of_mounts).to eq([hero3, hero1, hero2])
   end
 
   describe 'instance methods' do
@@ -27,7 +45,21 @@ RSpec.describe Hero do
 
       expect(hero1.mount_count).to eq(2)
     end
+  end
 
+  describe 'search' do
+    it 'name search' do
+      Hero.destroy_all
+      Mount.destroy_all
+      hero1 = Hero.create(name: "Hero", level: 8)
+      hero2 = Hero.create(name: "Andrew", level: 9)
+      hero3 = Hero.create(name: "Krono", level: 10)
+
+      expect(Hero.name_search('Andrew')).to eq([hero2])
+      expect(Hero.name_search('r')).to eq([hero1, hero2, hero3])
+      expect(Hero.name_search('sjpvj')).to eq([])
+      expect(Hero.name_search('')).to eq([hero1, hero2, hero3])
+    end
   end
 
 end
